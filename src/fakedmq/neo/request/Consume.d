3771081f47e95b5784dfff200c86a56432060218
davideckardt <david.eckardt@sociomantic.com>
2017-11-10 18:02:18 +0100

    Remove support for Consume v1 and v2

--- src/fakedmq/neo/request/Consume.d
+++ src/fakedmq/neo/request/Consume.d
@@ -51,18 +51,8 @@ public void handle ( Object shared_resources, RequestOnConn connection,
 
     switch ( cmdver )
     {
-        case 1:
-            scope rq = new ConsumeImpl_v1(resources);
-            rq.handle(connection, msg_payload);
-            break;
-
-        case 2:
-            scope rq = new ConsumeImpl_v2or3!(2)(resources);
-            rq.handle(connection, msg_payload);
-            break;
-
         case 3:
-            scope rq = new ConsumeImpl_v2or3!(3)(resources);
+            scope rq = new ConsumeImpl_v3(resources);
             rq.handle(connection, msg_payload);
             break;
 
@@ -80,168 +70,11 @@ public void handle ( Object shared_resources, RequestOnConn connection,
 
 /*******************************************************************************
 
-    Fake node implementation of the v1 Consume request protocol.
+    Fake node implementation of the v3 Consume request protocol.
 
 *******************************************************************************/
 
-private scope class ConsumeImpl_v1 : ConsumeProtocol_v1, DmqListener
-{
-    /***************************************************************************
-
-        Remember consumed channel
-
-    ***************************************************************************/
-
-    private Queue queue;
-
-    /***************************************************************************
-
-        Constructor.
-
-        Params:
-            shared_resources = DMQ request resources getter
-
-    ***************************************************************************/
-
-    public this ( IRequestResources resources)
-    {
-        super(resources);
-    }
-
-    /***************************************************************************
-
-        Performs any logic needed to start consuming from the channel of the
-        given name.
-
-        Params:
-            channel_name = channel to subscribe to
-            subscriber_name = subscriber name (v2 only)
-
-        Returns:
-            `true` if the channel may be used
-
-    ***************************************************************************/
-
-    override protected bool prepareChannel ( cstring channel_name )
-    {
-        this.queue = global_storage.getCreate(channel_name).subscribe("");
-        this.queue.register(this);
-        return true;
-    }
-
-    /***************************************************************************
-
-        Performs any logic needed to stop consuming from the channel of the
-        given name.
-
-        Params:
-            channel_name = channel to stop consuming from
-
-    ***************************************************************************/
-
-    override protected void stopConsumingChannel ( cstring channel_name )
-    {
-        this.queue.unregister(this);
-    }
-
-    /***************************************************************************
-
-        Retrieve the next value from the channel, if available.
-
-        Params:
-            value = buffer to write the value into
-
-        Returns:
-            `true` if there was a value in the channel, false if the channel is
-            empty
-
-    ***************************************************************************/
-
-    override protected bool getNextValue ( ref void[] value )
-    {
-        size_t records, bytes;
-        this.queue.countSize(records, bytes);
-
-        if ( records == 0 )
-            return false;
-
-        value = this.queue.pop().dup;
-        return true;
-    }
-
-    /***************************************************************************
-
-        Helper function to send a single value and handle messages received from
-        the client in the meantime.
-
-        (Note that this method is protected as implementations of the protocol
-        may wish to add extra behaviour at the point of sending a single value.)
-
-        Returns:
-            If the value was sent without a message being received,
-            State.Sending. Otherwise, the state to transition to (as determined
-            by the message received).
-
-    ***************************************************************************/
-
-    override protected State sendSingleValue ( )
-    {
-        auto next_state = super.sendSingleValue();
-
-        this.queue.consumerFlushed();
-
-        return next_state;
-    }
-
-    /***************************************************************************
-
-        DmqListener interface method. Called by Storage when new data arrives
-        or the channel is deleted.
-
-        Params:
-            code = trigger event code
-
-    ***************************************************************************/
-
-    override public void trigger ( Code code )
-    {
-        with ( Code ) switch ( code )
-        {
-            case DataReady:
-                this.dataReady();
-                break;
-            case Finish:
-                this.channelRemoved();
-                break;
-            default:
-                break;
-        }
-    }
-}
-
-/*******************************************************************************
-
-    Evaluates to `ConsumeProtocol_v2` or `ConsumeProtocol_v3` according to `v`
-    which should be either 2 or 3.
-
-*******************************************************************************/
-
-private template ConsumeProtocol_v2or3 ( uint v )
-{
-    static if (v == 2)
-        alias ConsumeProtocol_v2 ConsumeProtocol_v2or3;
-    else static if (v == 3)
-        alias ConsumeProtocol_v3 ConsumeProtocol_v2or3;
-}
-
-/*******************************************************************************
-
-    Fake node implementation of the v2 Consume request protocol.
-
-*******************************************************************************/
-
-private scope class ConsumeImpl_v2or3 ( uint v ) :
-    ConsumeProtocol_v2or3!(v), DmqListener
+private scope class ConsumeImpl_v3 : ConsumeProtocol_v3, DmqListener
 {
     /***************************************************************************
 
@@ -350,12 +183,9 @@ private scope class ConsumeImpl_v2or3 ( uint v ) :
                 this.dataReady();
                 break;
 
-            static if (v == 3)
-            {
-                case Flush:
-                    this.flushBatch();
-                    break;
-            }
+            case Flush:
+                this.flushBatch();
+                break;
 
             case Finish:
                 this.channelRemoved();
